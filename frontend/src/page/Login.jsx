0b0615578358d1490adf5cbe63649ec6092191d3
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-10-22 13:16:09 +0530

    url chnges

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
 
     setLoading(true);
     try {
-      const res = await axios.post(`http://localhost:5000/api/auth/login`, {
+      const res = await axios.post(`${API_BASE_URL}/auth/login`, {
         email,
         password,
       });
